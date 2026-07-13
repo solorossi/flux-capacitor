2f384fe873afb97ab887b469d95861e6d4472fab
Ross Greinke <ross.greinke@gmail.com>
2026-07-13 13:41:15 -0400

    Starter application from Spring Boot Initializer.

---
+++ src/main/java/org/solorossi/fluxcapacitor/FluxCapacitorApplication.java
@@ -0,0 +1,13 @@
+package org.solorossi.fluxcapacitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxCapacitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxCapacitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/solorossi/fluxcapacitor/FluxCapacitorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solorossi.fluxcapacitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxCapacitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
